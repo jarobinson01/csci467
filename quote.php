73ad2c9aac87f7af74671a1fe532ba3efb961368
jrobin15283 <jeremyakiva@gmail.com>
2023-04-23 15:21:15 -0500

    Update quote.php

--- quote.php
+++ quote.php
@@ -10,16 +10,16 @@
     echo '<h1>Quote #'.$rows[0]['quote_id'].'</h1>';
     echo '<form action=hq.php method=POST><button>BACK</button></form>';
     foreach($rows as $row){
-        $sql = "SELECT * FROM Quote WHERE id = ".$row['item_id'].";";
+        /*$sql = "SELECT * FROM Quote WHERE id = ".$row['item_id'].";";
         $prepared = $db1->prepare($sql);
         $success = $prepared->execute();
         $lineItem = $prepared->fetch();
-        $price = $lineItem['price'];
+        $price = $lineItem['price'];*/
 
         echo '<tr>';
         echo '<td>Item ID: '.$row['item_id'].'</td>';
         echo '<td> ---  Quantity: '.$row['quantity'].'</td>';
-        echo '<td> ---  Price: '.$row['quantity']*$price.'</td>';
+        //echo '<td> ---  Price: '.$row['quantity']*$price.'</td>';
         echo '</tr>';
         echo ' ---- ';
         echo '<button onClick="document.location.href=\'quote.php\'">Edit</button>';
