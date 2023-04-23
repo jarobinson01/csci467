51d58c1a4a8ae77c1253f443dffdb9a8be6b4b63
jrobin15283 <jeremyakiva@gmail.com>
2023-04-23 14:43:58 -0500

    Update quote.php

--- quote.php
+++ quote.php
@@ -13,7 +13,7 @@
         $sql = "SELECT * FROM Quote WHERE id = ".$row['item_id'].";";
         $prepared = $db1->prepare($sql);
         $success = $prepared->execute();
-        $lineItem = $prepared->fetchALL(PDO::FETCH_ASSOC);
+        $lineItem = $prepared->fetchALL();
         $price = $lineItem[0]['price'];
 
         echo '<tr>';
