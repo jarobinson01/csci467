aa443d25d5adc3eafa4673c081f3578fedd5a81b
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 17:08:44 -0500

    add item

--- add_item.php
+++ add_item.php
@@ -1,30 +1,24 @@
 <?php
     include('config.php');
 
-    $sql = "INSERT INTO Item (price, name) VALUES (?, ?);";
-    $prepared = $db1->prepare($sql);
-    $success = $prepared->execute(array($_POST['price'], $_POST['name']));
-
-    $sql = "SELECT * FROM Item;";
-    $prepared = $db1->prepare($sql);
-    $success = $prepared->execute();
-    $rows = $prepared->fetchALL(PDO::FETCH_ASSOC);
-    echo "<br>";
-    print_r($rows);
-
+    // Get current id value for the Item that will be added
     $sql = "SELECT `AUTO_INCREMENT`
             FROM  INFORMATION_SCHEMA.TABLES
             WHERE TABLE_SCHEMA = 'z1923374'
             AND   TABLE_NAME   = 'Item';";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute();
-    $rows = $prepared->fetch();
-    echo "<br>";
-    print_r($rows[0]);
+    $item_id = $prepared->fetch();
 
+    // Insert row into Item table
+    $sql = "INSERT INTO Item (price, name) VALUES (?, ?);";
+    $prepared = $db1->prepare($sql);
+    $success = $prepared->execute(array($_POST['price'], $_POST['name']));
+
+    // Insert row into Quote_Item table
     $sql = "INSERT INTO Quote_Item (quote_id, item_id) VALUES (?, ?);";
     $prepared = $db1->prepare($sql);
-    $success = $prepared->execute(array($_POST['quote_id'], $_POST['item_id']));
+    $success = $prepared->execute(array($_POST['quote_id'], $item_id[0]));
 
     $sql = "SELECT * FROM Quote_Item;";
     $prepared = $db1->prepare($sql);
