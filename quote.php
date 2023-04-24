d31d735c14cefacfb9c6913c009d74dcdd1244c8
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 10:48:12 -0500

    quote

--- quote.php
+++ quote.php
@@ -20,7 +20,7 @@
     include('config.php');
     $id = key($_POST);
 
-    $sql = "SELECT * FROM Quote_Item WHERE quote_id = ?";
+    $sql = "SELECT * FROM Quote_Item";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute(array($id));
 
