4e89ded18588ab8958fa90e492a30532e3a69315
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 12:53:53 -0500

    Update quote.php

--- quote.php
+++ quote.php
@@ -51,11 +51,11 @@
 
     echo '<button id="newItem" onclick="showAddItem()">New Item</button>';
     echo '<form id="addItem" action="add_item.php" method="POST" style="display: none;">';
+    echo '<input name="quote_id" value = "'.$id.'" style="diaplay: none;">';
     echo '<input placeholder="Item Name" name="item_id">';
-    echo '<input placeholder="Item Quantity">';
-    echo '<input name="quote_id" value = "'.$id.'"';// style="diaplay: none;"';
+    //echo '<input placeholder="Item Quantity">';
     echo '<input placeholder="Item Price" name="price">';
-    echo '<input type="submit" value="Add Item"/>';
+    echo '<input type="submit" value="Add Item">';
     echo '</form>';
 
     echo '<hr>';
