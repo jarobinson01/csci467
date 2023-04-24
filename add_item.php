29388f35ced8850a7fb4c13fc940fe55ac81613b
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 16:59:46 -0500

    Update add_item.php

--- add_item.php
+++ add_item.php
@@ -6,8 +6,12 @@
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute(array($_POST['price'], $_POST['name']));
 
-    $rows = $prepared->fetchALL(PDO::FETCH_ASSOC);
-    print_r($rows);
+    $sql = "SELECT `AUTO_INCREMENT`
+            FROM  INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_SCHEMA = 'z1923374'
+            AND   TABLE_NAME   = 'Item';";
+    $prepared = $db1->prepare($sql);
+    $success = $prepared->execute();
 
     $sql = "INSERT INTO Quote_Item (quote_id) VALUES (?, ?, ?);";
     $prepared = $db1->prepare($sql);
