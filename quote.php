fe0c42406a2c4b29b59aead2b6f8b13cb22a3da8
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 12:20:24 -0500

    Update quote.php

--- quote.php
+++ quote.php
@@ -18,7 +18,6 @@
 
 <?php
     include('config.php');
-    include('functions.php');
     $id = array(key($_POST));
 
     $sql = "SELECT * FROM Quote_Item WHERE quote_id = ?;";
@@ -26,8 +25,29 @@
     $success = $prepared->execute($id);
 
     $rows = $prepared->fetchALL(PDO::FETCH_ASSOC);
+    $total = 0;
+    //echo '<h1>Quote #'.$rows[0]['quote_id'].'</h1>';
+    echo '<form action=hq.php method=POST><button>BACK</button></form>';
+    foreach($rows as $row){
+        $sql = "SELECT * FROM Item WHERE id = ".$row['item_id'].";";
+        $prepared = $db1->prepare($sql);
+        $success = $prepared->execute();
+        $lineItem = $prepared->fetch();
+        $price = $lineItem['price'];
+        $total += $row['quantity']*$price;
 
-    display_quote($rows);
+        echo '<tr>';
+        echo '<td>Quote ID: '.$row['quote_id'].'</td>';
+        echo '<td> --- Item ID: '.$row['item_id'].'</td>';
+        //echo '<td> ---  Quantity: '.$row['quantity'].'</td>';
+        //echo '<td> ---  Price: $'.$row['quantity']*$price.'</td>';
+        echo '<td> ---  Price: $'.$price.'</td>';
+        echo '</tr>';
+        echo ' ---- ';
+        echo '<button onClick="document.location.href=\'quote.php\'">Edit</button>';
+        echo '<button>Delete</button>';
+        echo '</br>';
+    }
     
     function addItem() {
         $sql = "INSERT INTO Item (price) VALUES ('".$_POST['price']."');";
