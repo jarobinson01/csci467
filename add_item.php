bb84cf4d007e8d3ac6c67547e7db3a0574bc3ec6
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 16:52:51 -0500

    add item

--- add_item.php
+++ add_item.php
@@ -6,16 +6,12 @@
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute(array($_POST['price'], $_POST['name']));
 
-    $sql = "SELECT * FROM Item;";
-    $prepared = $db1->prepare($sql);
-    $success = $prepared->execute();
-
     $rows = $prepared->fetchALL(PDO::FETCH_ASSOC);
     print_r($rows);
 
-    /*$sql = "INSERT INTO Quote_Item (quote_id) VALUES (?, ?, ?);";
+    $sql = "INSERT INTO Quote_Item (quote_id) VALUES (?, ?, ?);";
     $prepared = $db1->prepare($sql);
-    $success = $prepared->execute(array($_POST['quote_id'], $_POST['price'], $_POST['name']));*/
+    $success = $prepared->execute(array($_POST['quote_id'], $_POST['price'], $_POST['name']));
     //print_r($prepared);
 
     //header("Location: quote.php");
--- quote.php
+++ quote.php
@@ -54,7 +54,7 @@
 
     echo '<button id="newItem" onclick="showAddItem()">New Item</button>';
     echo '<form id="addItem" action="add_item.php" method="POST" style="display: none;">';
-    echo '<input name="quote_id" value="'.$id.'" style="diaplay: none;">';
+    echo '<input type="hidden" name="quote_id" value="'.$id.'">';
     echo '<input placeholder="Item Name" name="name">';
     //echo '<input placeholder="Item Quantity">';
     echo '<input placeholder="Item Price" name="price">';
