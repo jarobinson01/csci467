1954958dd480d29af3a2cdd386efa3b7ce14c0ee
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 13:10:46 -0500

    quote id

--- add_item.php
+++ add_item.php
@@ -1,12 +1,11 @@
 <?php
     include('config.php');
-    print_r($_POST);
 
     $sql = "INSERT INTO Item (price) VALUES ('".$_POST['price']."');";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute();
 
-    $sql = "INSERT INTO Quote_Item (quote_id, item_id, price) VALUES ('".$_POST['quote_id']."', '".$_POST['item_id']."', '".$_POST['price']."');";
+    $sql = "INSERT INTO Quote_Item (quote_id, item_id, price) VALUES ('".$QUOTE_ID."', '".$_POST['item_id']."', '".$_POST['price']."');";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute();
 
--- quote.php
+++ quote.php
@@ -20,7 +20,6 @@
     include('config.php');
     $id = key($_POST);
     $QUOTE_ID = $id;
-    print_r($_POST);
 
     $sql = "SELECT * FROM Quote_Item WHERE quote_id = ?;";
     $prepared = $db1->prepare($sql);
@@ -53,7 +52,7 @@
 
     echo '<button id="newItem" onclick="showAddItem()">New Item</button>';
     echo '<form id="addItem" action="add_item.php" method="POST" style="display: none;">';
-    echo '<input name="quote_id" value="'.$id.'" style="diaplay: none;">';
+    //echo '<input name="quote_id" value="'.$id.'" style="diaplay: none;">';
     echo '<input placeholder="Item Name" name="item_id">';
     //echo '<input placeholder="Item Quantity">';
     echo '<input placeholder="Item Price" name="price">';
