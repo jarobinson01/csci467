fb2c2a303a402484cc63b1edd89e2cb0db01cde0
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 14:56:40 -0500

    Update add_item.php

--- add_item.php
+++ add_item.php
@@ -1,5 +1,6 @@
 <?php
     include('config.php');
+    echo "Hello";
 
     $sql = "INSERT INTO Item (price) VALUES (?);";
     $prepared = $db1->prepare($sql);
@@ -8,7 +9,7 @@
     $sql = "INSERT INTO Quote_Item (quote_id, item_id, price) VALUES (?, ?, ?);";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute(2, $_POST['item_id'], $_POST['price']);
-    //print_r($prepared);
+    print_r($prepared);
 
-    header("Location: quote.php");
+    //header("Location: quote.php");
 ?>
