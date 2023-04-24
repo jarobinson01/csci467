97bba928542f1bbb089403869739994108ec1482
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 17:02:15 -0500

    Update add_item.php

--- add_item.php
+++ add_item.php
@@ -12,10 +12,18 @@
             AND   TABLE_NAME   = 'Item';";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute();
+    $rows = $prepared->fetchALL(PDO::FETCH_ASSOC);
+    print_r($rows);
 
-    $sql = "INSERT INTO Quote_Item (quote_id) VALUES (?, ?, ?);";
+    //$sql = "INSERT INTO Quote_Item (quote_id) VALUES (?, ?, ?);";
+    //$prepared = $db1->prepare($sql);
+    //$success = $prepared->execute(array($_POST['quote_id'], $_POST['price'], $_POST['name']));
+
+    $sql = "SELECT * FROM Quote_Item;";
     $prepared = $db1->prepare($sql);
-    $success = $prepared->execute(array($_POST['quote_id'], $_POST['price'], $_POST['name']));
+    $success = $prepared->execute();
+    $rows = $prepared->fetchALL(PDO::FETCH_ASSOC);
+    print_r($rows);
     //print_r($prepared);
 
     //header("Location: quote.php");
