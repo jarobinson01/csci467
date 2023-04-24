a198f5050130bc1b13cc69ff0b6e2716ea3a7ad3
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 12:44:40 -0500

    Update quote.php

--- quote.php
+++ quote.php
@@ -20,7 +20,7 @@
     include('config.php');
     $id = key($_POST);
     echo $id;
-    echo $_POST;
+    print_r($_POST);
 
     $sql = "SELECT * FROM Quote_Item WHERE quote_id = ?;";
     $prepared = $db1->prepare($sql);
