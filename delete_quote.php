4f607cb6aab2efd9b1039cc1726d7a401d801976
jrobin15283 <jeremyakiva@gmail.com>
2023-04-25 14:50:58 -0500

    Update delete_quote.php

--- delete_quote.php
+++ delete_quote.php
@@ -5,10 +5,23 @@
 
     $quote_id = key($_POST);
 
-    // Delete specified quote from table
-    $sql = "DELETE FROM Quote WHERE quote_id = 1";
+    $sql = "DELETE FROM Create_Quote WHERE foreign_quote_id = ?";
     $prepared = $db1->prepare($sql);
-    $success = $prepared->execute();//array($quote_id));
+    $success = $prepared->execute(array($quote_id));
+
+    $sql = "DELETE FROM Quote_Note WHERE foreign_quote_id = ?";
+    $prepared = $db1->prepare($sql);
+    $success = $prepared->execute(array($quote_id));
+
+    $sql = "DELETE FROM Quote_Item WHERE foreign_quote_id = ?";
+    $prepared = $db1->prepare($sql);
+    $success = $prepared->execute(array($quote_id));
+
+    // Delete specified quote from table
+    $sql = "DELETE FROM Quote WHERE quote_id = ?";
+    $prepared = $db1->prepare($sql);
+    $success = $prepared->execute(array($quote_id));
+
     if($success) {
         echo "Success";
     } else {
