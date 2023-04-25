abebfd4c0d3adcbd67a7e8efdd92eb75b524ef3d
jrobin15283 <jeremyakiva@gmail.com>
2023-04-25 15:41:27 -0500

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -6,9 +6,9 @@
     $keys = array_keys($_POST);
     $_SESSION['ITEM_ID'] = $keys[2];
 
-    $sql = "UPDATE Item SET name=:name WHERE item_id=:id;";
+    $sql = "UPDATE Item SET item_name=:item_name WHERE item_id=:id;";
     $prepared = $db1->prepare($sql);
-    $prepared->execute(array('name' => $_POST['name'], 'id' => $_SESSION['ITEM_ID']));
+    $prepared->execute(array('item_name' => $_POST['name'], 'id' => $_SESSION['ITEM_ID']));
 
     $sql = "UPDATE Item SET price=:price WHERE item_id=:id;";
     $prepared = $db1->prepare($sql);
