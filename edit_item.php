b64518f3e37da83c80ef3f1cb11476a098a9c8d0
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 21:10:56 -0500

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -3,8 +3,11 @@
     
     include('config.php');
 
+    $_SESSION["QUOTE_ID"] = key($_POST[2]);
+
     $item_id = key($_POST);
     print_r($_POST);
+    echo "<br>";
 
     /*$sql = "UPDATE Item SET name=";
     $prepared = $db1->prepare($sql);
