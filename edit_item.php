b3e95de97764300ab1c1ffa254684288f05db91f
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 22:48:52 -0500

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -6,24 +6,20 @@
     $keys = array_keys($_POST);
     $_SESSION['ITEM_ID'] = $keys[2];
 
-    $sql = 'UPDATE Item SET name=:name WHERE id=:id;';
+    $sql = "UPDATE Item SET name=:name WHERE id=:id;";
     $prepared = $db1->prepare($sql);
     $prepared->execute(array('name' => $_POST['name'], 'id' => $_SESSION['ITEM_ID']));
 
-    /*$sql = "UPDATE Item SET name='?' WHERE id=?;";
+    $sql = "UPDATE Item SET price=:price WHERE id=:id;";
     $prepared = $db1->prepare($sql);
-    $prepared->execute(array($_POST['name'], $item_id));*/
+    $success = $prepared->execute(array('price' => $_POST['price'], 'id' => $_SESSION['ITEM_ID']));
 
-    /*$sql = "UPDATE Item SET price=".$_POST['price']." WHERE id=".$item_id.";";
-    $prepared = $db1->prepare($sql);
-    $success = $prepared->execute();*/
-
-    $sql = "SELECT * FROM Item;";
+    /*$sql = "SELECT * FROM Item;";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute();
     $rows = $prepared->fetchALL(PDO::FETCH_ASSOC);
     echo "<br>";
-    print_r($rows);
+    print_r($rows);*/
 
     header("Location: quote.php");
 ?>
