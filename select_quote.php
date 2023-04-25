ca7bb66971cd7f82c0a017cea9033ec3aed4535f
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 20:04:14 -0500

    Update select_quote.php

--- select_quote.php
+++ select_quote.php
@@ -1,7 +1,5 @@
 <?php
     include('config.php');
-    echo $QUOTE_ID;
     $QUOTE_ID = key($_POST);
-    echo $QUOTE_ID;
-    //header("Location: quote.php");
+    header("Location: quote.php");
 ?>
