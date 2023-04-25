56c18e45cc5f5793afccdcddc53644c61fc84230
jrobin15283 <jeremyakiva@gmail.com>
2023-04-25 14:17:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,20 @@
         echo "Connection to database failed: " . $e->getMessage();
     }
 
+    // get data from the SQL file
+	$query = file_get_contents("script.sql");
+
+	// prepare the SQL statements
+	$stmt = $db->prepare($query);
+
+	// execute the SQL
+	if ($stmt->execute()){
+		echo "Success";
+	}
+	else {
+		echo "Fail";
+	}
+
     if(!isset($_POST['name'])) {
         echo "<div class=\"center\">"; //centers the entire first page
             echo "<h1>Login</h1>"; //header for main page
