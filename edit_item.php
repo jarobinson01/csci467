8a4fc23ec899b8a63bf4d461061322db1dede329
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 21:37:01 -0500

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -5,14 +5,20 @@
 
     $keys = array_keys($_POST);
     $_SESSION["QUOTE_ID"] = $keys[2];
-
     $item_id = key($_POST);
-    print_r($_POST);
-    echo "<br>";
-    echo $_SESSION["QUOTE_ID"];
 
-    /*$sql = "UPDATE Item SET name=";
+    $sql = "UPDATE Item SET name='".$_POST['name']."' WHERE id='".$item_id."'";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute();
-    $item_id = $prepared->fetch();*/
+
+    $sql = "UPDATE Item SET price='".$_POST['price']."' WHERE id='".$item_id."'";
+    $prepared = $db1->prepare($sql);
+    $success = $prepared->execute();
+
+    $sql = "SELECT * FROM Item;";
+    $prepared = $db1->prepare($sql);
+    $success = $prepared->execute();
+    $rows = $prepared->fetchALL(PDO::FETCH_ASSOC);
+    echo "<br>";
+    print_r($rows);
 ?>
