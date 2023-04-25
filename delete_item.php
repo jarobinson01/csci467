1efa1821161b3bde10251f193aedd215f78aeb9f
jrobin15283 <jeremyakiva@gmail.com>
2023-04-25 15:10:25 -0500

    Update delete_item.php

--- delete_item.php
+++ delete_item.php
@@ -5,13 +5,13 @@
 
     $item_id = key($_POST);
 
-    // Insert row into Item table
-    $sql = "DELETE FROM Item WHERE item_id = ?;";
+    // Insert row into Quote_Item table
+    $sql = "DELETE FROM Quote_Item WHERE foreign_item_id = ?;";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute(array($item_id));
 
-    // Insert row into Quote_Item table
-    $sql = "DELETE FROM Quote_Item WHERE foreign_item_id = ?;";
+    // Insert row into Item table
+    $sql = "DELETE FROM Item WHERE item_id = ?;";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute(array($item_id));
 
