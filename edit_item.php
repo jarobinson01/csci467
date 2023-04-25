197beadbbe5de8d3da6cbcbe082a6015fda34a42
jrobin15283 <jeremyakiva@gmail.com>
2023-04-24 21:41:05 -0500

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -7,12 +7,12 @@
     $_SESSION["QUOTE_ID"] = $keys[2];
     $item_id = key($_POST);
 
-    //$sql = "UPDATE Item SET name='".$_POST['name']."' WHERE id='".$item_id."'";
-    $sql = "UPDATE Item SET name='Desk' WHERE id=1";
+    $sql = "UPDATE Item SET name='".$_POST['name']."' WHERE id=".$item_id.";";
+    //$sql = "UPDATE Item SET name='Desk' WHERE id=1";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute();
 
-    /*$sql = "UPDATE Item SET price=".$_POST['price']." WHERE id='".$item_id."'";
+    /*$sql = "UPDATE Item SET price=".$_POST['price']." WHERE id=".$item_id.";";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute();*/
 
