8e3a3634e1a291dfd99e799f8079aaa61393c1a6
jrobin15283 <jeremyakiva@gmail.com>
2023-04-25 23:54:06 -0500

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -6,6 +6,14 @@
     $keys = array_keys($_POST);
     $_SESSION['ITEM_ID'] = $keys[2];
 
+    // Store old item price for quote price calculations
+    $sql = "SELECT * FROM Item WHERE item_id=:id;";
+    $prepared = $db1->prepare($sql);
+    $prepared->execute(array('id' => $_SESSION['ITEM_ID']));
+    $item = $prepared->fetch();
+    $old_price = $item['price'];
+    $diff = $_POST['price'] - $old_price;
+
     // Update item name
     $sql = "UPDATE Item SET item_name=:item_name WHERE item_id=:id;";
     $prepared = $db1->prepare($sql);
@@ -17,14 +25,6 @@
     $prepared->execute(array('price' => $_POST['price'], 'id' => $_SESSION['ITEM_ID']));
 
     // Update quote price
-    $sql = "SELECT * FROM Item WHERE item_id=:id;";
-    $prepared = $db1->prepare($sql);
-    $prepared->execute(array('id' => $_SESSION['ITEM_ID']));
-    $item = $prepared->fetch();
-    $old_price = $item['price'];
-    echo $old_price."<br>";
-    $diff = $_POST['price'] - $old_price;
-
     $sql = "SELECT * FROM Quote WHERE quote_id=?;";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute(array($_SESSION['QUOTE_ID']));
@@ -32,12 +32,11 @@
 
     $price = $quote['price'];
     $price = $price + $diff;
-    echo $diff;
 
     $sql = "UPDATE Quote SET price=:price WHERE quote_id=:id;";
     $prepared = $db1->prepare($sql);
     $prepared->execute(array('price' => $price, 'id' => $_SESSION['QUOTE_ID']));
 
     // Redirect to quote page
-    //header("Location: quote.php");
+    header("Location: quote.php");
 ?>
