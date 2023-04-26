9cf01668f3a31e780593f7ab027c875c05d777ab
jrobin15283 <jeremyakiva@gmail.com>
2023-04-25 20:55:02 -0500

    sanction quote functionality complete

--- sanction_quote.php
+++ sanction_quote.php
@@ -3,9 +3,9 @@
     
     include('config.php');
 
-    $sql = "UPDATE Quote SET status='Sanctioned' WHERE item_id=:id;";
+    $sql = "UPDATE Quote SET status='Sanctioned' WHERE quote_id=:id;";
     $prepared = $db1->prepare($sql);
-    $prepared->execute(array('id' => $_SESSION['ITEM_ID']));
+    $prepared->execute(array('id' => $_SESSION['QUOTE_ID']));
 
     header("Location: hq.php");
 ?>
