a8c08bb20fb734c01831b3c13b12c87d231da4d4
jrobin15283 <jeremyakiva@gmail.com>
2023-04-26 17:14:42 -0500

    Update edit_note.php

--- edit_note.php
+++ edit_note.php
@@ -5,11 +5,19 @@
 
     $keys = array_keys($_POST);
     $_SESSION['NOTE_ID'] = $keys[1];
+    print_r($keys."<br>");
 
     // Update item name
     $sql = "UPDATE Note SET text_field=? WHERE note_id=:?";
     $prepared = $db1->prepare($sql);
-    $prepared->execute(array($_POST['note'], $_SESSION['NOTE_ID']));
+    $success = $prepared->execute(array($_POST['note'], $_SESSION['NOTE_ID']));
+
+    if ($success){
+		echo "Success";
+	}
+	else {
+		echo "Fail";
+	}
 
     // Redirect to quote page
     //header("Location: quote.php");
