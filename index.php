069c5d5cc41e15b45b9e6cf58af2a684c8af2776
jrobin15283 <jeremyakiva@gmail.com>
2023-04-26 17:23:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,21 +9,7 @@
     } catch(PDOexception $e) { // handle that exception
         echo "Connection to database failed: " . $e->getMessage();
     }
-
-    // get data from the SQL file
-	$query = file_get_contents("script.sql");
-
-	// prepare the SQL statements
-	$stmt = $db1->prepare($query);
-
-	// execute the SQL
-	if ($stmt->execute()){
-		echo "Success";
-	}
-	else {
-		echo "Fail";
-	}
-
+    
     if(!isset($_POST['name'])) {
         echo "<div class=\"center\">"; //centers the entire first page
             echo "<h1>Login</h1>"; //header for main page
