8015c52dd7bfb7fc50ee8a7a43cb5aa506fac87a
jrobin15283 <jeremyakiva@gmail.com>
2023-04-26 09:36:29 -0500

    Update delete_item.php

--- delete_item.php
+++ delete_item.php
@@ -5,6 +5,13 @@
 
     $item_id = key($_POST);
 
+    // Store item price for quote price calculations
+    $sql = "SELECT * FROM Item WHERE item_id=:id;";
+    $prepared = $db1->prepare($sql);
+    $prepared->execute(array('id' => $_SESSION['ITEM_ID']));
+    $item = $prepared->fetch();
+    $item_price = $item['price'];
+
     // Insert row into Quote_Item table
     $sql = "DELETE FROM Quote_Item WHERE foreign_item_id = ?;";
     $prepared = $db1->prepare($sql);
@@ -14,6 +21,19 @@
     $sql = "DELETE FROM Item WHERE item_id = ?;";
     $prepared = $db1->prepare($sql);
     $success = $prepared->execute(array($item_id));
+
+    // Update quote price
+    $sql = "SELECT * FROM Quote WHERE quote_id=?;";
+    $prepared = $db1->prepare($sql);
+    $success = $prepared->execute(array($_SESSION['QUOTE_ID']));
+    $quote = $prepared->fetch();
+
+    $price = $quote['price'];
+    $price = $price - $item_price;
+
+    $sql = "UPDATE Quote SET price=:price WHERE quote_id=:id;";
+    $prepared = $db1->prepare($sql);
+    $prepared->execute(array('price' => $price, 'id' => $_SESSION['QUOTE_ID']));
 
     // Redirect to quote page
     header("Location: quote.php");
