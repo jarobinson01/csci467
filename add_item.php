fe8b0bf078478d46cfc7e717440350c37c4bddee
jrobin15283 <jeremyakiva@gmail.com>
2023-04-26 10:29:56 -0500

    Update add_item.php

--- add_item.php
+++ add_item.php
@@ -6,6 +6,10 @@
     //echo $_POST['price']."<br>";
     //echo number_format($_POST['price'], 2, '.', '');
 
+    $item_price = $_POST['price'];
+    $item_price = number_format($item_price, 2, '.', '');
+    echo $item_price;
+
     // Get current id value for the Item that will be added
     $sql = "SELECT `AUTO_INCREMENT`
             FROM  INFORMATION_SCHEMA.TABLES
@@ -18,7 +22,7 @@
     // Insert row into Item table
     $sql = "INSERT INTO Item (price, item_name) VALUES (?, ?);";
     $prepared = $db1->prepare($sql);
-    $success = $prepared->execute(array($_POST['price'], $_POST['name']));
+    $success = $prepared->execute(array($item_price, $_POST['name']));
 
     // Insert row into Quote_Item table
     $sql = "INSERT INTO Quote_Item (foreign_quote_id, foreign_item_id) VALUES (?, ?);";
@@ -31,13 +35,13 @@
     $success = $prepared->execute(array($_SESSION['QUOTE_ID']));
     $quote = $prepared->fetch();
 
-    $price = (float)$quote['price'];
-    $price = $price + $_POST['price'];
+    $price = $quote['price'];
+    $price = $price + $item_price;
 
     $sql = "UPDATE Quote SET price=:price WHERE quote_id=:id;";
     $prepared = $db1->prepare($sql);
     $prepared->execute(array('price' => $price, 'id' => $_SESSION['QUOTE_ID']));
 
     // Redirect to quote page
-    header("Location: quote.php");
+    //header("Location: quote.php");
 ?>
