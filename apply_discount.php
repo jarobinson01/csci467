42330bd4dd6464ae89efbac2ad2154d4169642c8
jrobin15283 <jeremyakiva@gmail.com>
2023-04-25 20:40:38 -0500

    apply discount functionality complete

--- apply_discount.php
+++ apply_discount.php
@@ -12,22 +12,15 @@
 
     $price = $quote['price'];
 
-    echo $_POST['discount']."<br>";
-    echo $price."<br>";
-
-    echo $_POST['discount_type']."<br>";
-
     if($_POST['discount_type'] == "percentage") {
         $price = $price * (100 - $_POST['discount']) / 100;
     } else {
         $price = $price - $_POST['discount'];
     }
 
-    echo $price."<br>";
-
     $sql = "UPDATE Quote SET price=:price WHERE quote_id=:id;";
     $prepared = $db1->prepare($sql);
     $prepared->execute(array('price' => $price, 'id' => $_SESSION['QUOTE_ID']));
 
-    //header("Location: quote.php");
+    header("Location: quote.php");
 ?>
