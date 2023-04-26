38a9c3e4be76480e03c237fd2382b37c4218b25e
jrobin15283 <jeremyakiva@gmail.com>
2023-04-26 10:04:15 -0500

    Update edit_item.php

--- edit_item.php
+++ edit_item.php
@@ -6,13 +6,21 @@
     $keys = array_keys($_POST);
     $_SESSION['ITEM_ID'] = $keys[2];
 
+    $new_price = $_POST['price'];
+
     // Store old item price for quote price calculations
     $sql = "SELECT * FROM Item WHERE item_id=:id;";
     $prepared = $db1->prepare($sql);
     $prepared->execute(array('id' => $_SESSION['ITEM_ID']));
     $item = $prepared->fetch();
     $old_price = $item['price'];
-    $diff = $_POST['price'] - $old_price;
+
+    // Reject negative values
+    if($new_price < 0) {
+        $new_price = $old_price;
+    }
+
+    $diff = $new_price - $old_price;
 
     // Update item name
     $sql = "UPDATE Item SET item_name=:item_name WHERE item_id=:id;";
@@ -22,7 +30,7 @@
     // Update item price
     $sql = "UPDATE Item SET price=:price WHERE item_id=:id;";
     $prepared = $db1->prepare($sql);
-    $prepared->execute(array('price' => $_POST['price'], 'id' => $_SESSION['ITEM_ID']));
+    $prepared->execute(array('price' => $new_price, 'id' => $_SESSION['ITEM_ID']));
 
     // Update quote price
     $sql = "SELECT * FROM Quote WHERE quote_id=?;";
@@ -30,12 +38,12 @@
     $success = $prepared->execute(array($_SESSION['QUOTE_ID']));
     $quote = $prepared->fetch();
 
-    $price = $quote['price'];
-    $price = $price + $diff;
+    $quote_price = $quote['price'];
+    $quote_price = $quote_price + $diff;
 
     $sql = "UPDATE Quote SET price=:price WHERE quote_id=:id;";
     $prepared = $db1->prepare($sql);
-    $prepared->execute(array('price' => $price, 'id' => $_SESSION['QUOTE_ID']));
+    $prepared->execute(array('price' => $quote_price, 'id' => $_SESSION['QUOTE_ID']));
 
     // Redirect to quote page
     header("Location: quote.php");
