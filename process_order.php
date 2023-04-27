8380f6369468658d9cbdd856afd03c75e0144a06
jrobin15283 <jeremyakiva@gmail.com>
2023-04-27 15:39:47 -0500

    Update process_order.php

--- process_order.php
+++ process_order.php
@@ -16,14 +16,38 @@
             )
         );
 
+        // Add to external processing system
         $context  = stream_context_create($options);
         $result = file_get_contents($url, false, $context);
 
+        // Grab comission from JSON array
         $data = json_decode($result,true);
         $commission = $data['commission'];
 
         $commission = str_replace('%', '', $commission) / 100.00;
         $commission = (int)$commission;
+        
+        // Update process comission percent
+        $sql = "SELECT * FROM Processed_Quote WHERE foreign_quote_id = ?;";
+        $prepared = $db1->prepare($sql);
+        $success = $prepared->execute(array($_SESSION['QUOTE_ID']));
+        $processed_quote = $prepared->fetch();
+
+        $sql = "SELECT * FROM Processed WHERE process_id = ?;";
+        $prepared = $db1->prepare($sql);
+        $success = $prepared->execute(array($processed_quote['foreign_process_id']));
+        $processed = $prepared->fetch();
+
+        $sql = "UPDATE Processed SET commission_percent=:pct WHERE process_id=:id;";
+        $prepared = $db1->prepare($sql);
+        $prepared->execute(array('pct' => $commission, 'id' => $processed['process_id']));
+
+        $sql = "SELECT * FROM Processed WHERE process_id = ?;";
+        $prepared = $db1->prepare($sql);
+        $success = $prepared->execute(array($processed_quote['foreign_process_id']));
+        $processed = $prepared->fetch();
+
+        print_r($processed);
 
         echo($result."<br>");
     }
