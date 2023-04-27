b3931dfd0578561f64c9094e1506f29224fe902a
jrobin15283 <jeremyakiva@gmail.com>
2023-04-27 11:42:31 -0500

    Update process_order.php

--- process_order.php
+++ process_order.php
@@ -18,6 +18,6 @@
 
         $context  = stream_context_create($options);
         $result = file_get_contents($url, false, $context);
-        echo($result);
+        echo($result."<br>");
     }
 ?>
